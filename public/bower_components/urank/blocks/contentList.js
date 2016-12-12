27484193f0ac2ffeffd1056382e2cd569cbc5280
Jorge Guerra <jorge.guerra881215@gmail.com>
2016-12-12 10:26:54 -0300

    Add Urank Instructive

--- public/bower_components/urank/blocks/contentList.js
+++ public/bower_components/urank/blocks/contentList.js
@@ -93,7 +93,7 @@ var ContentList = (function(){
             event.stopPropagation(); s.onWatchiconClicked.call(this, event.data);
         };
         var onFaviconClick = function(event){
-            event.stopPropagation(); s.onFaviconClicked.call(this, event.data);
+            /*event.stopPropagation(); s.onFaviconClicked.call(this, event.data);*/
         };
 
         $li.off({
